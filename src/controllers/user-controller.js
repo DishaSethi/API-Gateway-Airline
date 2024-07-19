76edc00c64bb6313b3ba61e4a6fed4a789a6813c
Disha <sethidisha123@gmail.com>
2024-07-19 21:24:44 +0530

    successfully addded user creating logic

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,40 @@
+// const { StatusCodes } = require('http-status-codes');
+const {StatusCodes}=require('http-status-codes');
+
+const { UserService } = require('../services');
+const { SuccessResponse, ErrorResponse } = require('../utils/common');
+
+
+/**
+ * POST : /signup
+ * req-body {email: 'a@b.com',password:'1234'}
+ */
+async function signup(req, res) {
+ 
+    try {
+    //   console.log(req.body);
+
+        // if (!req.body || !req.body.email || !req.body.password) {
+        //     throw new Error('Email or password missing in request body');
+        // }
+        const user = await UserService.create({
+            email: req.body.email,
+            password: req.body.password
+        });
+        SuccessResponse.data = user;
+        return res
+                .status(StatusCodes.CREATED)
+                .json(SuccessResponse);
+    } catch(error) {
+        // console.log(error);
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
+
+
+module.exports = {
+   signup
+}
