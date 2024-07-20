b2a28dfe96817eb20168ff35e868743eb4d15d65
Disha <sethidisha123@gmail.com>
2024-07-21 01:58:04 +0530

    added JWT expiry token handling

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -59,7 +59,12 @@ async function isAuthenticated(token){
         }
         return user.id;
     } catch (error) {
-        if(err instanceof AppError) throw error;
+        if(error instanceof AppError) throw error;
+
+        if(error.name=='TokenExpiredError'){
+        throw new AppError('JWT token expired',StatusCodes.BAD_REQUEST);
+
+        }
         if(error.name == 'JsonWebTokenError'){
         throw new AppError('Invalid JWT token',StatusCodes.BAD_REQUEST);
 
